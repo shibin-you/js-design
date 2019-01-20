83bb4f419ac3bd39ae55bf012cd74c0bcd215187
shibin-you <709594315@qq.com>
2019-01-20 18:11:33 +0800

    test

---
+++ mds/.vuepress/config.js
@@ -0,0 +1,31 @@
+const resolve=d=>require('path').resolve(__dirname,d)
+module.exports = {
+  title: 'javascript design',
+  description: 'javascript design',
+  base: '/jsDesign/',
+  dest:resolve('../../docs'),
+  head: [
+         ['link', { rel: 'icon', href: `/logo.png` }]
+     ],
+     themeConfig: {
+       nav: [],
+         sidebar: {
+           '/book': [
+             {
+               title: '第一章',
+                collapsable: false,
+                children: [
+                         'book/1/1.md',
+                         'book/1/2.md',
+                         'book/1/3.md',
+                     ]
+             },
+
+             {
+               title:'第二章',
+               collapsable: false
+             },
+           ]
+         }
+       }
+}
