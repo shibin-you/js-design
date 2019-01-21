2f09ff5ca1f83425b020ca40eb5ed09eac4017d8
shibin-you <709594315@qq.com>
2019-01-21 14:09:58 +0800

    test

--- mds/.vuepress/config.js
+++ mds/.vuepress/config.js
@@ -1,31 +1,36 @@
-const resolve=d=>require('path').resolve(__dirname,d)
+const resolve = d => require('path').resolve(__dirname, d)
 module.exports = {
   title: 'javascript design',
   description: 'javascript design',
   base: '/js-design/',
-  dest:resolve('../../docs'),
+  dest: resolve('../../docs'),
   head: [
-         ['link', { rel: 'icon', href: `/logo.png` }]
-     ],
-     themeConfig: {
-       nav: [],
-         sidebar: {
-           '/book': [
-             {
-               title: '第一章',
-                collapsable: false,
-                children: [
-                         'book/1/1.md',
-                         'book/1/2.md',
-                         'book/1/3.md',
-                     ]
-             },
+    ['link', {
+      rel: 'icon',
+      href: `/logo.png`
+    }]
+  ],
+  themeConfig: {
+    nav: [],
+    sidebar: {
+      '/book': [{
+          title: '第一章',
+          collapsable: false,
+          children: [
+            'book/1/1.md',
+            'book/1/2.md',
+            'book/1/3.md',
+          ]
+        },
 
-             {
-               title:'第二章',
-               collapsable: false
-             },
-           ]
-         }
-       }
+        {
+          title: '第二章',
+          collapsable: false,
+          children: [
+            'book/2/1.md'
+          ]
+        },
+      ]
+    }
+  }
 }
